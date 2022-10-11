c9e0a904e35755f56f66baac6c204e9064a3894f
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-10-11 09:49:01 -0400

    commit tree dfcd8c13702c2c272b3c752dd86a89ff8aa96286

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = (num1 * num2);
+
+let random = (Math.ceil(Math.random()*10));
+
+const num3 = 19
+const num4 = 5
+let mod = (num3 % num4);
+
+let max = (Math.max(1,20,3,4,5,2,6));
